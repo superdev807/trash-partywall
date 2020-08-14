dfbc03f9d825bb0d8e260e8259fec9400d848817
superdev807 <charles.ferria21@gmail.com>
2020-08-14 13:37:52 -0400

    #3 Connection to database implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const config = require("./config/keys");
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+connectDB();
 
 app.listen(config.SERVER_PORT, () => {
   console.log(`Server listening on port ${config.SERVER_PORT}`);
